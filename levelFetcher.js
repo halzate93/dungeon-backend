26ff4f9b7b403068d2352ffaa0930c5edc4a019f
Laura Garcia <lauraga41@gmail.com>
2016-06-04 13:45:16 -0500

    Connection to database.

--- levelFetcher.js
+++ levelFetcher.js
@@ -1,8 +1,26 @@
-
+var url = 'mongodb://localhost:27017/dungeon';
+var mongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
 var getLevels = function(req, res)
 {
-	 res.send('{"monsters": [{"monsterType": "Spider","attackPoints": 5,"defensePoints": 10,"lifePoints": 100}]}');
+	mongoClient.connect(url, function(err, db) {
+		findLevels(db, "castle", function(levels){
+			res.send(JSON.stringify(levels[0]));
+		});
+	});
 };
+ 
+var findLevels = function(db, id, callback) {
+  // Get the documents collection 
+  var collection = db.collection('levels');
+  // Find some documents 
+  collection.find({"levelId": id}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.dir(docs);
+    callback(docs);
+  });
+}
 
 exports.getLevels = getLevels;
